6f571133b35b910ff2a60d289faa0248ff632973
delu <delu@zuosi.com>
2018-09-01 12:42:40 +0800

    添加了gunicorn配置, 用于自动化部署的一部分

--- v3/app.sh
+++ v3/app.sh
@@ -1,2 +1,20 @@
 #!/bin/sh
-python /apps/web/blog_python/v3/index.py 9000 >> /apps/web/logs/blog.log &
+
+command=$1
+LOG_DIR=/apps/web/logs
+
+if [ "$command" = "restart" ]; then
+    echo "restart"
+    kill -HUP `cat $LOG_DIR/gunicorn.pid`
+
+elif [ "$command" = "stop" ]; then
+    echo "stop"
+    kill -QUIT `cat $LOG_DIR/gunicorn.pid`
+
+elif [ "$command" = "start" ]; then
+    echo "start"
+    gunicorn -c /apps/web/blog_python/v3/gunicorn.py index:app -D --capture-output --enable-stdio-inheritance
+
+else
+    echo "usage: $0 start|stop|restart"
+fi
---
+++ v3/gunicorn.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+
+"""
+@author: delu
+@file: gunicorn.py.py
+@time: 18/9/1 12:23
+"""
+import gevent.monkey
+gevent.monkey.patch_all()
+import multiprocessing
+bind = '0.0.0.0:9000'      #绑定ip和端口号
+backlog = 512                #监听队列
+timeout = 30      #超时
+worker_class = 'tornado' #使用gevent模式，还可以使用sync 模式，默认的是sync模式
+pidfile = '/apps/web/logs/gunicorn.pid'
+workers = multiprocessing.cpu_count() * 2 + 1    #进程数
+threads = 2 #指定每个进程开启的线程数
+loglevel = 'info' #日志级别，这个日志级别指的是错误日志的级别，而访问日志的级别无法设置
+accesslog = "/apps/web/logs/gunicorn_access.log"      #访问日志文件
+errorlog = "/apps/web/logs/gunicorn_error.log"        #错误日志文件
